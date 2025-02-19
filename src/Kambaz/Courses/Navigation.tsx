05c208fdf06bf7d9864f11fdeb071a495358c5eb
maxbolz <maxfbolz@gmail.com>
2025-02-18 20:02:50 -0500

    resolve issue with appended assignment id

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -6,7 +6,7 @@ export default function CourseNavigation() {
     return (
         <ListGroup id="wd-courses-navigation" className="wd list-group fs-5 me-5 rounded-0">
             {links.map((link) => (
-                <Link to={`${pathname.replace(pathname.split("/")[4], link)}`} id="wd-course-home-link" className={`list-group-item border border-0 ${pathname.includes(link) ? "active" : "text-danger"}`}>{link}</Link>
+                <Link to={`${pathname.split("/").slice(0, 4).join("/")}/${link}`}  id="wd-course-home-link" className={`list-group-item border border-0 ${pathname.includes(link) ? "active" : "text-danger"}`}>{link}</Link>
             ))}
         </ListGroup>
     );
