b2c819904a2c69cd70f72228dd688ce36e430b43
maxbolz <maxfbolz@gmail.com>
2025-02-18 19:55:46 -0500

    remove unused import

--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -1,5 +1,5 @@
 import { Table } from "react-bootstrap";
-import { FaUserCircle, FaPaw } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import * as db from "../../Database";
 export default function PeopleTable() {
