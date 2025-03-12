164db30f0e56e1a334f568be1e9750d2c0c80556
maxbolz <maxfbolz@gmail.com>
2025-03-12 00:20:53 -0400

    actually rewrite dashboard

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -10,8 +10,8 @@ export default function Dashboard() {
     const { currentUser } = useSelector((state: any) => state.accountReducer);
     const { courses } = useSelector((state: any) => state.courseReducer);
     const filteredCourses = courses.filter((course: any) => enrollments.some((enrollment) => enrollment.user === currentUser._id && enrollment.course === course._id));
-
     const dispatch = useDispatch();
+
     const [curCourse, setCurCourse] = useState<any>({
         _id: "0", name: "New Course", number: "New Number",
         startDate: "2023-09-10", endDate: "2023-12-15",
@@ -19,6 +19,12 @@ export default function Dashboard() {
     });
     const [courseName, setCourseName] = useState(curCourse.name);
     const [courseDescription, setCourseDescription] = useState(curCourse.description);
+
+    const updateCourseWithCheck = () => {
+        if (!curCourse)
+            return;
+        dispatch(updateCourse({ ...curCourse, name: courseName, description: courseDescription, editing: false }));
+    };
 
     return (
         <div id="wd-dashboard">
@@ -30,23 +36,7 @@ export default function Dashboard() {
                         id="wd-add-new-course-click"
                         onClick={() => dispatch(addCourse({ name: courseName, description: courseDescription }))} > Add </button>
                     <button className="btn btn-warning float-end me-2"
-                        onClick={() => {
-
-                            if (!curCourse)
-                                return;
-
-                            dispatch(updateCourse({
-                                _id: curCourse._id,
-                                name: courseName,
-                                description: courseDescription,
-                                number: curCourse.number,
-                                startDate: curCourse.startDate,
-                                endDate: curCourse.endDate,
-                                department: curCourse.department,
-                                credits: curCourse.credits
-                            }));
-
-                        }} id="wd-update-course-click">
+                        onClick={updateCourseWithCheck} id="wd-update-course-click">
                         Update
                     </button>
                 </div>
