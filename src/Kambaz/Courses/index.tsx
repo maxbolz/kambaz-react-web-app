a6c221922bdf7f977b00396494282b11a2f1f233
maxbolz <maxfbolz@gmail.com>
2025-03-12 00:23:25 -0400

    update courses

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -6,10 +6,11 @@ import AssignmentEditor from "./Assignments/Editor";
 import PeopleTable from "./People/Table";
 import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import { FaAlignJustify } from "react-icons/fa";
-import { courses } from "../Database";
+import { useSelector } from "react-redux";
 export default function Courses() {
     const { cid } = useParams();
-    const course = courses.find((course) => course._id === cid);
+    const { courses } = useSelector((state: any) => state.courseReducer);
+    const course = courses.find((course: any) => course._id === cid);
     const { pathname } = useLocation();
     return (
         <div id="wd-courses">
