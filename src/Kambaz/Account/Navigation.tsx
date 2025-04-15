a9dadbeb5f3d36944e1ea9b9b1090a12a3ae32b6
maxbolz <maxfbolz@gmail.com>
2025-04-15 15:52:14 -0400

    initial

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 export default function AccountNavigation() {
 
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
 
   return (links.map((link) =>
     <div id="wd-courses-navigation" className="wd list-group fs-5 me-5 rounded-0">
-      <Link to={`/Kambaz/Account/${link}`} className="list-group-item active border border-0"> {link} </Link> <br />
+      <Link to={`/Kambaz/Account/${link}`} className={`list-group-item ${active(link)} border border-0`}> {link} </Link> <br />
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link to={`/Kambaz/Account/Users`} className={`list-group-item ${active("Users")} border border-0`}> Users </Link>)}
     </div>
   ));
 }
