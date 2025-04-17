fb1813fbd8551ab6cec61907059972af2f19f440
maxbolz <maxfbolz@gmail.com>
2025-04-17 15:44:25 -0400

    cascading changes

--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -1,22 +1,8 @@
 import { Table } from "react-bootstrap";
-import { useParams } from "react-router";
-import { useState, useEffect } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import PeopleDetails from "./Details";
 import { Link } from "react-router-dom";
-import * as client from "../../Account/client";
-export default function PeopleTable() {
-
-    const { cid, uid } = useParams();
-    const [users, setUsers] = useState<any[]>([]);
-    const fetchUsers = async () => {
-        const users = await client.findUsersForCourse(cid as string);
-        setUsers(users);
-    };
-    useEffect(() => {
-        fetchUsers();
-    }, [uid]);
-
+export default function PeopleTable({ users = [] }: { users?: any[]}) {
     return (
         <div id="wd-people-table">
             <PeopleDetails />
